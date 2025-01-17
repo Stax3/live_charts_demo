ff15214dd41d9cc87234c8ab3189a1d027dbd994
Stax3 Engineering <engineering@stax3.com>
2025-01-17 11:23:31 +0500

    Cleanup: Reformat codebase

--- test/live_charts_demo_web/controllers/error_html_test.exs
+++ test/live_charts_demo_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule LiveChartsDemoWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(LiveChartsDemoWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(LiveChartsDemoWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
--- test/live_charts_demo_web/controllers/error_json_test.exs
+++ test/live_charts_demo_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule LiveChartsDemoWeb.ErrorJSONTest do
   use LiveChartsDemoWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert LiveChartsDemoWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert LiveChartsDemoWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
